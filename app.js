dd97b5efb65053d65295f8a187597f61c5b89634
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-10-17 08:05:39 -0600

    Creating a new game

--- app.js
+++ app.js
@@ -9,6 +9,27 @@ GAME RULES:
 
 */
 
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
+
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
@@ -22,19 +43,11 @@ function nextPlayer() {
     document.querySelector('.dice').style.display = 'none';
 }
 
-let scores = [0, 0];
-let roundScore = 0;
-let activePlayer = 0;
+let scores;
+let roundScore;
+let activePlayer;
 
-let x = document.querySelector('#score-0').textContent;
-console.log(x);
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-0').textContent = '0';
+init();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -77,3 +90,5 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     }
 
 });
+
+document.querySelector('.btn-new').addEventListener('click', init);
